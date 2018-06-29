6e431c67453f2752019a1d2332cd497de4e76661
Alison Shin <alisonyshin@gmail.com>
2018-06-29 08:01:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,8 +4,8 @@ def valid_move?(board,index)
   if (index >= 0) && (index <= board.length - 1)
       return TRUE
   end
-  if position_taken?(board,index) != FALSE
-      return TRUE
+  if position_taken?(board,index) = FALSE
+      return FALSE
   end
 end
 
